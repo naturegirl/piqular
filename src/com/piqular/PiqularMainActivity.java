e78e5e3e3346fa959feb48434b92d6ae8d8068ea
Jennifer Guo <jjguo@princeton.edu>
2014-04-06 08:18:54 -0400

    removing "settings" button

--- src/com/piqular/PiqularMainActivity.java
+++ src/com/piqular/PiqularMainActivity.java
@@ -198,9 +198,6 @@ public class PiqularMainActivity extends ActionBarActivity {
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
